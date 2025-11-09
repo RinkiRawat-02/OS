a0c38fc490a1e02981c2aed48023989ab56aba6e
RinkiRawat-02 <rinkirawat.rr02@gmail.com>
2025-11-09 12:04:42 +0530

    First Fit Memory Allocation

---
+++ Week8_ques2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+    int b, p;
+    printf("Enter number of blocks: ");
+    scanf("%d", &b);
+
+    int block[b];
+    for (int i = 0; i < b; i++)
+        scanf("%d", &block[i]);
+
+    printf("Enter number of processes: ");
+    scanf("%d", &p);
+
+    int process[p], allocation[p];
+    for (int i = 0; i < p; i++)
+    {
+        scanf("%d", &process[i]);
+        allocation[i] = -1;
+    }
+
+    for (int i = 0; i < p; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            if (block[j] >= process[i])
+            {
+                allocation[i] = j;
+                block[j] -= process[i];
+                break;
+            }
+        }
+    }
+
+    printf("\nAllocation Result (First Fit):\n");
+    for (int i = 0; i < p; i++)
+    {
+        if (allocation[i] != -1)
+            printf("Process %d allocated to Block %d\n", i + 1, allocation[i] + 1);
+        else
+            printf("Process %d: no free block available\n", i + 1);
+    }
+    return 0;
+}
