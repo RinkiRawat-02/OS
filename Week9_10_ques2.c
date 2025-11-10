fcb9247e251fdbc3d46a8c2ed337550b32036a12
RinkiRawat-02 <rinkirawat.rr02@gmail.com>
2025-11-10 20:23:17 +0530

    Linked File Allocation

---
+++ Week9_10_ques2.c
@@ -0,0 +1,67 @@
+//Linked File Allocation
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 50
+
+struct File
+{
+    char name[20];
+    int start;
+    int size;
+    int blocks[MAX];
+};
+
+int main()
+{
+    int n;
+    char search[20];
+    struct File f[MAX];
+
+    printf("Enter number of files: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("\nEnter file name: ");
+        scanf("%s", f[i].name);
+
+        printf("Enter starting block number: ");
+        scanf("%d", &f[i].start);
+
+        printf("Enter number of blocks required: ");
+        scanf("%d", &f[i].size);
+
+        printf("Enter the %d block numbers (in order): ", f[i].size);
+        for (int j = 0; j < f[i].size; j++)
+        {
+            scanf("%d", &f[i].blocks[j]);
+        }
+    }
+
+    printf("\nEnter file name to search: ");
+    scanf("%s", search);
+
+    for (int i = 0; i < n; i++)
+    {
+        if (strcmp(search, f[i].name) == 0)
+        {
+            printf("\nFile Found ✅\n");
+            printf("File Name: %s\n", f[i].name);
+            printf("Starting Block (Head): %d\n", f[i].start);
+            printf("Number of Blocks: %d\n", f[i].size);
+
+            printf("Linked Blocks: ");
+            for (int j = 0; j < f[i].size; j++)
+            {
+                printf("%d -> ", f[i].blocks[j]);
+            }
+            printf("NULL\n");
+            return 0;
+        }
+    }
+
+    printf("\nFile Not Found ❌\n");
+    return 0;
+}
